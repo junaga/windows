a89377ae4f1d3a0e377dfbc81d083b9b97b436b3
Hermann Stanew <hermann-stanew@invita.gmbh>
2023-02-03 03:26:45 +0100

    fmt

--- wsl/wsl.bash
+++ wsl/wsl.bash
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Timezone can not be changed easy. it's an open issue
+# https://github.com/microsoft/WSL/issues/6417
+
 # change the WSL Debian VM timezone to UTC
 echo "UTC" | tee /etc/timezone
 ln -sf /usr/share/zoneinfo/UTC /etc/localtime
