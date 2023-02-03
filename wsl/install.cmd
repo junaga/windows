12616c4c2a974a685f8707c4325fffafb5af8415
Hermann Stanew <hermann-stanew@invita.gmbh>
2023-02-03 03:25:07 +0100

    new script to re-install and apply config

---
+++ wsl/install.cmd
@@ -0,0 +1,24 @@
+@ECHO OFF
+
+ECHO uninstalling previous Debian
+wsl.exe --unregister Debian
+
+ECHO installing Debian
+wsl.exe --install Debian
+
+ECHO:
+ECHO Windows needs to mount the "network location"
+ECHO sleeping 10 seconds...
+@REM this is how we `sleep` in windows
+@REM https://stackoverflow.com/questions/1672338/how-to-sleep-for-five-seconds-in-a-batch-file-cmd
+PING 127.0.0.1 -n 11 > nul
+
+@REM place config files
+copy %~dp0\wsl.conf \\wsl.localhost\Debian\etc\wsl.conf
+copy %~dp0\wsl.bash \\wsl.localhost\Debian\etc\opt\wsl.bash
+echo @reboot root bash /etc/opt/wsl.bash >> \\wsl.localhost\Debian\etc\crontab
+
+@REM reboot to apply configuration
+wsl.exe --shutdown
+
+PAUSE
